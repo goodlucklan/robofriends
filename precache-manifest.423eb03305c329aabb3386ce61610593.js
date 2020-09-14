dcd186cec1129b7cdb9de79f4346ad548d6a68f7
Luis Enrique Roman Pardo <43737954+goodlucklan@users.noreply.github.com>
2020-09-13 22:40:03 -0500

    Updates

---
+++ precache-manifest.423eb03305c329aabb3386ce61610593.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86300441ea2ce9ffc2b0a140c47c7f26",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "438e167281383ad04726",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "66ca85ff69c3a3e9d071",
+    "url": "/robofriends/static/css/main.a3c3f630.chunk.css"
+  },
+  {
+    "revision": "438e167281383ad04726",
+    "url": "/robofriends/static/js/2.07c9a331.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.07c9a331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ca85ff69c3a3e9d071",
+    "url": "/robofriends/static/js/main.a3d10425.chunk.js"
+  },
+  {
+    "revision": "1c43dc848485329a93b3",
+    "url": "/robofriends/static/js/runtime-main.5da0a606.js"
+  }
+]);
